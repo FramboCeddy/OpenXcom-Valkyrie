e94a99a73c6fb89f307cf0093c4b89de2cbb8dfd
FramboCeddy <162596471+FramboCeddy@users.noreply.github.com>
2026-04-17 14:35:13 +0200

    revert commit #61fcd5d6 Special search filter for only researched topics is unnecessary as research diary already shows this.

--- src/Basescape/TechTreeSelectState.cpp
+++ src/Basescape/TechTreeSelectState.cpp
@@ -232,13 +232,6 @@ void TechTreeSelectState::initLists()
 				continue;
 			}
 		}
-		else if (searchString == "MARCOPOLO")
-		{
-			if (!_parent->isDiscoveredResearch(res))
-			{
-				continue;
-			}
-		}
 		else if (projectName.find(searchString) == std::string::npos)
 		{
 			continue;
@@ -260,13 +253,6 @@ void TechTreeSelectState::initLists()
 		if (searchString == "SHAZAM")
 		{
 			if (_parent->isDiscoveredManufacture(manuf))
-			{
-				continue;
-			}
-		}
-		else if (searchString == "MARCOPOLO")
-		{
-			if (!_parent->isDiscoveredManufacture(manuf))
 			{
 				continue;
 			}
@@ -296,13 +282,6 @@ void TechTreeSelectState::initLists()
 		if (searchString == "SHAZAM")
 		{
 			if (_parent->isDiscoveredFacility(facType))
-			{
-				continue;
-			}
-		}
-		else if (searchString == "MARCOPOLO")
-		{
-			if (!_parent->isDiscoveredFacility(facType))
 			{
 				continue;
 			}
@@ -341,13 +320,6 @@ void TechTreeSelectState::initLists()
 				continue;
 			}
 		}
-		else if (searchString == "MARCOPOLO")
-		{
-			if (!_parent->isProtectedAndDiscoveredItem(itemType))
-			{
-				continue;
-			}
-		}
 		else if (itemName.find(searchString) == std::string::npos)
 		{
 			continue;
@@ -373,13 +345,6 @@ void TechTreeSelectState::initLists()
 		if (searchString == "SHAZAM")
 		{
 			if (_parent->isDiscoveredCraft(craftType))
-			{
-				continue;
-			}
-		}
-		else if (searchString == "MARCOPOLO")
-		{
-			if (!_parent->isDiscoveredCraft(craftType))
 			{
 				continue;
 			}
